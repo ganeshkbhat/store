91e994d168083438659057e7a5bb0da66b8d60f4
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-12-21 18:05:05 +0530

    [store] Adding initial commit

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@
  * Package: 
  * Author: Ganesh B
  * Description: Nodejs npm module to work with immutable stores. Manage immutable stores with no jazz in a simple manner. Exploring simpler immutable stores architectures
- * Install: npm i stores --save
+ * Install: npm i immutablesjs --save
  * Github: https://github.com/ganeshkbhat/store
  * npmjs Link: https://www.npmjs.com/package/store
  * File: store.js

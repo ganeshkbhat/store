56caff32c294b177ea27ffe7294532bad14503e1
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-12-21 18:54:11 +0530

    [store] Adding initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Package: 
  * Author: Ganesh B
  * Description: Nodejs npm module to work with immutable stores. Manage immutable stores with no jazz in a simple manner. Exploring simpler immutable stores architectures
- * Install: npm i immutablesjs --save
+ * Install: npm i mutables --save
  * Github: https://github.com/ganeshkbhat/store
  * npmjs Link: https://www.npmjs.com/package/store
  * File: index.js
---
+++ test/test.store.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: Nodejs npm module to work with immutable stores. Manage immutable stores with no jazz in a simple manner. Exploring simpler immutable stores architectures
+ * Install: npm i mutables --save
+ * Github: https://github.com/ganeshkbhat/store
+ * npmjs Link: https://www.npmjs.com/package/store
+ * File: test.store.js
+ * File Description: 
+ * 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';

d6607d918116f2d4182dc618fe1f8d7c377aa15a
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-07-31 17:48:30 +0530

    Update object.keys.array.search.js
    update file with file comments

--- src/object.keys.array.search.js
+++ src/object.keys.array.search.js
@@ -1,3 +1,22 @@
+/**
+ * 
+ * Package: mutables
+ * Author: Ganesh B
+ * Description: Nodejs npm module to work with immutable stores. Manage immutable stores with no jazz in a simple manner. Exploring simpler immutable stores architectures
+ * Install: npm i mutables --save
+ * Github: https://github.com/ganeshkbhat/store
+ * npmjs Link: https://www.npmjs.com/package/store
+ * File: store.js
+ * File Description: 
+ * 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
 const { expect } = require('chai');
 const sinon = require('sinon');
 
